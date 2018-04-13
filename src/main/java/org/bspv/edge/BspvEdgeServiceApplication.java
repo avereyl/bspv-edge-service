efb887018dca3d1090b341236ad32a2a9dba04ee
Guillaume Billaud <guillaume.billaud@dachser.com>
2018-04-13 16:36:06 +0200

    Starting edge-service project

---
+++ src/main/java/org/bspv/edge/BspvEdgeServiceApplication.java
@@ -0,0 +1,12 @@
+package org.bspv.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspvEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BspvEdgeServiceApplication.class, args);
+	}
+}
