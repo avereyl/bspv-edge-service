861b02a255a0826f43e79f64d928c943d97e9f8f
Guillaume Billaud <guillaume.billaud@dachser.com>
2018-06-05 17:41:43 +0200

    adding https support + starting configuration of zuul & eureka

--- src/main/java/org/bspv/edge/BspvEdgeServiceApplication.java
+++ src/main/java/org/bspv/edge/BspvEdgeServiceApplication.java
@@ -2,7 +2,9 @@ package org.bspv.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class BspvEdgeServiceApplication {
 
